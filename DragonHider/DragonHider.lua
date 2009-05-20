4bffe11cf2290f38cd7869149f435b411ce00c62
Sven Kirmess <sven.kirmess@kzone.ch>
2009-05-20 20:09:36 +0000

    DragonHider-1
    git-svn-id: https://dragon-hider.googlecode.com/svn/trunk@2 b0ae2bc8-4578-11de-a3b0-c1e83b3b5a46

---
+++ DragonHider/DragonHider.lua
@@ -0,0 +1,15 @@
+
+local DragonHider_Version = 1
+
+function DragonHider_OnEvent(event)
+
+	if ( event == "PLAYER_ENTERING_WORLD" ) then
+		this:UnregisterEvent("PLAYER_ENTERING_WORLD")
+
+		MainMenuBarLeftEndCap:Hide()
+		MainMenuBarRightEndCap:Hide()
+
+		DEFAULT_CHAT_FRAME:AddMessage(string.format("DragonHider %i loaded.", DragonHider_Version ))
+	end
+end
+
