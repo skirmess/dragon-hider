018b93d3abf8f046a02e938d4940dbc7858e8afe
Sven Kirmess <sven.kirmess@kzone.ch>
2010-07-05 20:21:20 +0000

    version 5
    git-svn-id: https://dragon-hider.googlecode.com/svn/tags/DragonHider-5@22 b0ae2bc8-4578-11de-a3b0-c1e83b3b5a46

---
+++ DragonHider/DragonHider.lua
@@ -0,0 +1,22 @@
+
+-- Copyright (c) 2009-2010, Sven Kirmess
+
+local Version = 5
+
+local function EventHandler(self, event, ...)
+
+	if ( event == "PLAYER_ENTERING_WORLD" ) then
+		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+
+		MainMenuBarLeftEndCap:Hide()
+		MainMenuBarRightEndCap:Hide()
+
+		DEFAULT_CHAT_FRAME:AddMessage(string.format("DragonHider %i loaded.", Version ))
+	end
+end
+
+-- main
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", EventHandler)
+
